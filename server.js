b48f977ab9a526a46ed646f92a5a45688212a814
marlowe <marlowepdx@gmail.com>
2022-07-13 15:20:24 -0700

    ? '

--- server.js
+++ server.js
@@ -46,6 +46,11 @@ function filterByQuery(query, animalsArray) {
   }
 
 
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
 
 
 app.get('/api/animals', (req, res) => {
@@ -55,6 +60,16 @@ app.get('/api/animals', (req, res) => {
    }
    res.json(results);
 });
+
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+      if (result) {
+          res.json(result);
+      } else {
+          res.send(404);
+      }
+  });
 
 
 
